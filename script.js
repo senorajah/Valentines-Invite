6ee048b9d59de8ae6f7eb3a4a538dd8aa9ef875e
Rajah <rajah.seno065@gmail.com>
2026-01-22 02:04:34 +0800

    letter content design

--- script.js
+++ script.js
@@ -8,18 +8,21 @@ nextBTN.addEventListener("click", () => {
     landing.classList.add("hidden");
     letter.classList.remove("hidden");
 
+    setTimeout(() => {
+     flap.style.transform = "rotateX(180deg)";
+    }, 50); 
 
-    flap.style.transform = "rotateX(180deg)";
+    setTimeout(() => {})
     letterContent.style.opacity = "1";
-})
+  },600);
+
+    
 
 const hearts = document.querySelectorAll('.heart');
 
 hearts.forEach((heart) => {
-  // Random horizontal position
+
   heart.style.left = Math.random() * 100 + 'vw';
-  // Random animation duration
   heart.style.animationDuration = 3 + Math.random() * 4 + 's';
-  // Optional: random scale
-  heart.style.fontSize = 45 + Math.random() * 50 + 'px';
+  heart.style.fontSize = 80 + Math.random() * 100 + 'px';
 });
